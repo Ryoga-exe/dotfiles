65b371d676b33f5a2a151f43a9ac72325d2ae633
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-03-13 02:35:50 +0900

    [powershell] feat: update powershell config

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,15 +2,41 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 $env:LESSCHARSET = "utf-8"
 
-# Prompt
+# Completion
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Terminal Icons
+Import-Module -Name Terminal-Icons
+
+# Starship
+$env:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+Invoke-Expression (&starship init powershell)
 
 # Neovim
 $env:XDG_CONFIG_HOME = "~/.config"
 
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+# winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
 
+# scoop
+Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+
+# Alias
+Set-Alias vim nvim
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\user\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\user\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
