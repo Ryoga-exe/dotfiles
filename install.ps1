1e2464a1a6ba9010a156b8c80c337bea68e6bb66
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-03-30 16:30:53 +0900

    feat: update install.ps1

---
+++ install.ps1
@@ -0,0 +1,29 @@
+Write-Host "Install dotfiles..."
+
+Write-Host "Set location to ~\"
+Set-Location $env:USERPROFILE
+
+Write-Host "Create workspace directories..."
+New-Item -ItemType "directory" -Path "$env:USERPROFILE\workspace" -Force | Out-Null
+New-Item -ItemType "directory" -Path "$env:USERPROFILE\workspace\repos" -Force | Out-Null
+New-Item -ItemType "directory" -Path "$env:USERPROFILE\workspace\research" -Force | Out-Null
+New-Item -ItemType "directory" -Path "$env:USERPROFILE\workspace\sandbox" -Force | Out-Null
+New-Item -ItemType "directory" -Path "$env:USERPROFILE\workspace\cp" -Force | Out-Null
+
+
+if (!(Get-Command -Name winget -ErrorAction SilentlyContinue)) {
+    Write-Warning "This script needs winget to install packages, please install winget."
+    exit
+}
+
+if (!(Get-Command -Name git -ErrorAction SilentlyContinue)) {
+    Write-Host "Install git..."
+    winget install -e --id Git.Git -h --accept-package-agreements --accept-source-agreements
+}
+
+Write-Host "Clone dotfiles repository..."
+git clone "https://github.com/Ryoga-exe/dotfiles.git" "$env:USERPROFILE\workspace\repos\dotfiles"
+
+Write-Host "Set Location to ~\workspace\repos\dotfiles"
+Set-Location "$env:USERPROFILE\workspace\repos\dotfiles"
+
