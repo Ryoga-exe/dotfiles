f6672cac0d411c564e793520463f55be94276f7c
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-05-07 02:12:50 +0900

    [nvim] feat: load by platform

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,17 @@ require('ryoga-exe.core.options')
 require('ryoga-exe.core.keymaps')
 require('ryoga-exe.core.colorsheme')
 
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac == 1 then
+    require('ryoga-exe.platform.macos')
+end
+if is_win == 1 then
+    require('ryoga-exe.platform.windows')
+end
+if is_wsl == 1 then
+    require('ryoga-exe.platform.wsl')
+end
