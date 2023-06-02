e5f02a5b322f29e2ea587e7e482c3ed12ed02231
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-02 13:47:33 +0900

    [nvim] feat: add some keymap and options

---
+++ .config/nvim/lua/ryoga-exe/core/keymaps.lua
@@ -0,0 +1,4 @@
+local keymap = vim.keymap
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
---
+++ .config/nvim/lua/ryoga-exe/core/options.lua
@@ -0,0 +1,19 @@
+local opt = vim.opt
+
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+opt.number = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smarttab = true
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
