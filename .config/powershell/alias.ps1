a18050b13efcd53d822490b4e93c191e5c44f7cd
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-17 05:59:40 +0900

    [powershell] feat: add alias CC

--- .config/powershell/alias.ps1
+++ .config/powershell/alias.ps1
@@ -1,6 +1,7 @@
 # Alias
 Set-Alias vim nvim
 Set-Alias grep findstr
+Set-Alias CC Set-Clipboard
 Set-Alias ll Get-ChildItem
 Set-Alias tig 'C:\Program Files\Git\user\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\user\bin\less.exe'
