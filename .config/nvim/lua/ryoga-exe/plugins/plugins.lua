8397b7f9e2d8ed54eb4955b264f18903c0464261
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-08-26 12:03:46 +0900

    [nvim] feat: add nvim-treesitter

--- .config/nvim/lua/ryoga-exe/plugins/plugins.lua
+++ .config/nvim/lua/ryoga-exe/plugins/plugins.lua
@@ -8,5 +8,38 @@ return {
                 -- ...
             })     
         end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        event = {'BufNewFile', 'BufRead'},
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = {
+                    "typescript",
+                    "javascript",
+                    "rust",
+                    "python",
+                    "go",
+                    "lua",
+                    "bash",
+                    "html",
+                    "css",
+                    "vue",
+                    "vim",
+                    "yaml",
+                    "toml",
+                    "ini",
+                    "json",
+                    "dockerfile",
+                    "markdown",
+                    "diff",
+                    "gitignore"
+                },
+                highlight = {
+                    enable = true,
+                },
+            }
+        end
     }
 }
