c0ec4b89b3b1ba221eb120e36d635d56511326a7
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-10-10 16:11:23 +0900

    [nvim] feat: add platform linux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,11 +6,15 @@ require('ryoga-exe.core.colorscheme')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
+local is_linux = has "unix"
 local is_win = has "win32"
 local is_wsl = has "wsl"
 
 if is_mac == 1 then
     require('ryoga-exe.platform.macos')
+end
+if is_linux == 1 then
+    require('ryoga-exe.platform.linux')
 end
 if is_win == 1 then
     require('ryoga-exe.platform.windows')
