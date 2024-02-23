b798a4736d824a2c8157902dc8506d73ec63a910
Ryoga-exe <git@ryoga.dev>
2024-02-24 01:56:54 +0900

    [nvim] feat: add wakatime plugin

--- .config/nvim/lua/user/plugins/init.lua
+++ .config/nvim/lua/user/plugins/init.lua
@@ -1,8 +1,14 @@
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.zig" },
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.astro" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.pack.zig" },
+  },
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
 }
