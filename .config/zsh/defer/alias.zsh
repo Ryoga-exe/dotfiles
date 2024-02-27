83778e44603f487e316bbc5b403029f5307f4979
Ryoga-exe <git@ryoga.dev>
2024-02-28 06:54:42 +0900

    [zsh] feat: add alias for ghq

--- .config/zsh/defer/alias.zsh
+++ .config/zsh/defer/alias.zsh
@@ -21,6 +21,18 @@ if [[ $(command -v nvim) ]]; then
     alias vi=nvim
 fi
 
+# ghq
+if [[ $(command -v ghq) ]]; then
+    function repos () {
+        local selected_dir=$(ghq list -p | fzf --layout=reverse --border --no-sort +m --query "$LBUFFER" --prompt "go to repo >")
+        if [ -n "$selected_dir" ]; then
+            cd $selected_dir
+        fi
+    }
+    bindkey '^]' repos
+    alias rp=repos
+fi
+
 alias v='fd --type f --hidden | fzf --layout=reverse --info=inline --border --no-sort +m --query "$LBUFFER" --prompt "Open with nvim >" --preview "if [ $(command -v bat) ]; then bat --color=always {}; elif [ $(command -v batcat) ]; then batcat --color=always {}; else cat {}; fi" | xargs nvim'
 alias -g CC='xclip -selection c'
 alias cd-gitroot='cd "$(git rev-parse --show-toplevel)"'
