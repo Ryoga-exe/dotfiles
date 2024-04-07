3b962f0f82662ddbe8e38c6643f6bc57a1a38d61
Ryoga-exe <git@ryoga.dev>
2024-04-07 20:22:12 +0900

    [dotter] feat: delete hook scripts

--- .dotter/pre_deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "Setup AstroNvim..."
-
-if [ -d ~/.config/nvim ]; then
-    echo "~/.config/nvim exists, skipped..."
-else
-    git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-fi
