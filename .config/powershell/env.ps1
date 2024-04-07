e94a6e8aef134f0938c41a64f5237a217a78d577
Ryoga-exe <git@ryoga.dev>
2024-04-07 16:16:10 +0900

    [powershell] fix: starship location

--- .config/powershell/env.ps1
+++ .config/powershell/env.ps1
@@ -1,4 +1,4 @@
 # Env
 $env:LESSCHARSET = "utf-8"
-$env:STARSHIP_CONFIG = "$env:XDG_CONFIG_HOME\starship.toml"
+$env:STARSHIP_CONFIG = "$env:XDG_CONFIG_HOME\starship\starship.toml"
 $env:GIT_SSH = "C:\Windows\System32\OpenSSH\ssh.exe"
