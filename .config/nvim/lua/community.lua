21e10112c575e4b05f33e53f8fd459619930e4c7
Ryoga-exe <git@ryoga.dev>
2024-04-07 20:30:19 +0900

    [nvim] feat: add community modules

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,12 +1,11 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
