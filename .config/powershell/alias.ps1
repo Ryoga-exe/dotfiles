fd952b97a6d57933fefadb592b5b230f0a45885d
Ryoga-exe <git@ryoga.dev>
2024-04-07 22:33:38 +0900

    [powershell] feat: update alias

--- .config/powershell/alias.ps1
+++ .config/powershell/alias.ps1
@@ -1,12 +1,11 @@
 # Alias
 Set-Alias vim nvim
+Set-Alias vi nvim
 Set-Alias grep findstr
 Set-Alias CC Set-Clipboard
 Set-Alias ls ezals
 Set-Alias ll ezall
 Set-Alias lt ezalt
 Set-Alias ltl ezaltl
-Set-Alias tig 'C:\Program Files\Git\user\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\user\bin\less.exe'
 Set-Alias cd. cdgitroot
 
