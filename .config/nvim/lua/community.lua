1dcd948c72c581e944b596c1cbe1b87710d43b57
Ryoga-exe <git@ryoga.dev>
2024-08-01 07:15:34 +0900

    [nvim] feat: add typst plugins

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -10,4 +10,5 @@ return {
   { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.typst" },
 }
